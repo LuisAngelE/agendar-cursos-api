9b4c91161fe2ffbbfbdce99493a997a55b69891c
LuisAngelE <luisangelem.dp@gmail.com>
2025-10-13 17:56:06 -0600

    helper

--- app/helpers.php
+++ app/helpers.php
@@ -3,8 +3,8 @@
 if (!function_exists('getBaseUrl')) {
     function getBaseUrl()
     {
-        //return 'http://localhost:3000/';
-        return 'https://pic.ldrhumanresources.com/';
+        return 'http://localhost:3000/';
+        //return 'https://pic.ldrhumanresources.com/';
         //return 'https://testcursos.ldrhumanresources.com/';
     }
 }
